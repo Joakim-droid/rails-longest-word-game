427b4fcc57dba01c3e0faf6eb8ed4edd0e10e908
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2020-08-13 21:16:14 +0200

    longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,29 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
-  def new; end
-  def score; end
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def score
+    @word = params[:word]
+    @letters = params[:letters].gsub(' ', '').split('')
+    if included?(@letters, @word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{@word}")
+    if JSON.parse(response.read)['found']
+      if an_english_word?(@word)
+        raise
+        @score = 'congrats!'
+      else
+        @score = '  not an english word'
+      end
+    else
+      @score = "#{@word} can't be built from"
+    end
+  end
+
+  def included?(letters, word)
+    word.split('').all? { |letter| letters.include? letter }
+  end
 end
